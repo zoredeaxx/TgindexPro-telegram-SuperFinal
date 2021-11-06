63fa232b4909ee56b29827050529a5bf9efd4598
_dev_goyal__ <65810521+dgbkn@users.noreply.github.com>
2021-11-06 13:52:45 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,33 +15,67 @@ from .config import host, port, session_string, api_id, api_hash, debug
 log = logging.getLogger(__name__)
 
 
+# class Indexer:
+
+#     TEMPLATES_ROOT = pathlib.Path(__file__).parent / 'templates'
+
+#     def __init__(self):
+#         self.server = web.Application()
+#         self.loop = asyncio.get_event_loop()
+#         self.tg_client = Client(session_string, api_id, api_hash)
+
+
+#     async def startup(self):
+#         await self.tg_client.start()
+#         log.debug("telegram client started!")
+
+#         await setup_routes(self.server, Views(self.tg_client))
+
+#         loader = jinja2.FileSystemLoader(str(self.TEMPLATES_ROOT))
+#         aiohttp_jinja2.setup(self.server, loader=loader)
+
+#         self.server.on_cleanup.append(self.cleanup)
+
+
+#     async def cleanup(self, *args):
+#         await self.tg_client.disconnect()
+#         log.debug("telegram client disconnected!")
+
+
+#     def run(self):
+#         self.loop.run_until_complete(self.startup())
+#         web.run_app(self.server, host=host, port=port)
+
+
+
 class Indexer:
 
-    TEMPLATES_ROOT = pathlib.Path(__file__).parent / 'templates'
+    TEMPLATES_ROOT = pathlib.Path(__file__).parent / "templates"
 
     def __init__(self):
-        self.server = web.Application()
+        middlewares = []
         self.loop = asyncio.get_event_loop()
+
+        self.server = web.Application()
+
+        self.server.on_startup.append(self.startup)
+        self.server.on_cleanup.append(self.cleanup)
+
         self.tg_client = Client(session_string, api_id, api_hash)
 
 
-    async def startup(self):
+    async def startup(self, server: web.Application):
         await self.tg_client.start()
         log.debug("telegram client started!")
 
-        await setup_routes(self.server, Views(self.tg_client))
+        await setup_routes(server, Views(self.tg_client))
 
         loader = jinja2.FileSystemLoader(str(self.TEMPLATES_ROOT))
-        aiohttp_jinja2.setup(self.server, loader=loader)
+        aiohttp_jinja2.setup(server, loader=loader)
 
-        self.server.on_cleanup.append(self.cleanup)
-
-
-    async def cleanup(self, *args):
+    async def cleanup(self, server: web.Application):
         await self.tg_client.disconnect()
         log.debug("telegram client disconnected!")
 
-
     def run(self):
-        self.loop.run_until_complete(self.startup())
-        web.run_app(self.server, host=host, port=port)
+        web.run_app(self.server, host=host, port=port, loop=self.loop)
